e99d23efca779a562333b35dcd3c967cce4be483
isamelmohamedbouh <ismaelmedbouh@outlook.fr>
2021-02-09 05:26:26 +0100

    API_GRAPH: Ajout de la gestion des erreurs globalisés

--- api_GRAPH/app.js
+++ api_GRAPH/app.js
@@ -1,5 +1,6 @@
 require('dotenv');
 const express = require('express');
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 const { moduleFormationRoute } = require('./routes');
 
 const app = express();
@@ -8,5 +9,7 @@ app.use(express.json());
 
 app.use('/api-graph/module-formation', moduleFormationRoute);
 
+app.use(notFound);
+app.use(errorHandler);
 const port = process.env.APP_PORT || 5000;
 module.exports = app;
