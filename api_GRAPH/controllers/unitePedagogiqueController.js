5e9b6e0c4dd2653b8f3a722db7b9808ccb9d04c3
isamelmohamedbouh <ismaelmedbouh@outlook.fr>
2021-02-10 11:43:09 +0100

    API_GRAPH: Récupération des unité pédagoqique par module de formation

--- api_GRAPH/controllers/unitePedagogiqueController.js
+++ api_GRAPH/controllers/unitePedagogiqueController.js
@@ -1,7 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const neode = require('../config/db');
 
-// @desc    Recupère tout les unités pédagogique par module de formation
+// @desc    Recupère tout les unités pédagogique par module de formation et dans l'ordre
 // @route   GET /unite-pedagogique/:id/par-module-pedagogique
 // @access  Private: Enseignant
 const getAllUnitePedagogiqueByModuleFormation = asyncHandler(
@@ -10,16 +10,27 @@ const getAllUnitePedagogiqueByModuleFormation = asyncHandler(
     const {
       records: unitePedagogique,
     } = await neode.cypher(
-      'MATCH (mf:ModuleFormation) - [:COMMENCE_PAR] -> (nn:UnitePedagogique) - [:SUIS*] -> (n:UnitePedagogique) WHERE mf.identifiant_module_formation = $id RETURN nn,n',
+      'MATCH (mf:ModuleFormation) - [:COMMENCE_PAR] -> (nn:UnitePedagogique) - [s:SUIS*] -> (n:UnitePedagogique) WHERE mf.identifiant_module_formation = $id RETURN nn,n,s',
       { id: parseInt(id) }
     );
     if (unitePedagogique.length === 0) {
+      res.status(404);
       throw new Error('Aucunes unités pédagogiques encore disponible');
     }
-    const unitePedagogiqueToArray = [unitePedagogique[0].get('nn').properties];
-    for (const up of unitePedagogique) {
-      unitePedagogiqueToArray.push(up.get('n').properties);
-    }
+    const unitePedagogiqueToArray = [
+      {
+        ...unitePedagogique[0].get('nn').properties,
+        ...unitePedagogique[0].get('s')[0].properties,
+      },
+    ];
+    unitePedagogique.forEach((up, index) => {
+      const suivant = unitePedagogique[index + 1]?.get('s')[index + 1]
+        ?.properties || { identifiant_unite_pedagogique_suivant: null };
+      unitePedagogiqueToArray.push({
+        ...up.get('n').properties,
+        ...suivant,
+      });
+    });
     res.json(unitePedagogiqueToArray);
   }
 );
--- api_GRAPH/tests/unitePedagogiqueRoutes.spec.js
+++ api_GRAPH/tests/unitePedagogiqueRoutes.spec.js
@@ -36,5 +36,27 @@ describe("Point d'entrée des modules de fomation", () => {
       `${URL}/unite-pedagogique/1/par-module-formation`
     );
     expect(res.statusCode).toEqual(200);
+    const { body } = res;
+    expect(body).toBeDefined();
+    for (
+      let i = 0;
+      body[i].identifiant_unite_pedagogique_suivant != null;
+      i++
+    ) {
+      expect(body[i].identifiant_unite_pedagogique_suivant).toEqual(
+        body[i + 1].id
+      );
+    }
+  });
+
+  it("GET /api-graph/unite-pedagogique/:id/par-module-formation n'existe pas", async () => {
+    const res = await request(app).get(
+      `${URL}/unite-pedagogique/546546/par-module-formation`
+    );
+    expect(res.statusCode).toEqual(404);
+    const { body } = res;
+    expect(body).toMatchObject({
+      message: 'Aucunes unités pédagogiques encore disponible',
+    });
   });
 });
