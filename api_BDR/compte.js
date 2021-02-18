7dc67d36bdea449f379d711d21dc5e7a6d42b462
isamelmohamedbouh <ismaelmedbouh@outlook.fr>
2021-02-18 17:52:06 +0100

    API_BDR: Ajoute des variables d'environnements

--- api_BDR/compte.js
+++ api_BDR/compte.js
@@ -1,85 +1,85 @@
+require('dotenv');
 const mysql = require('mysql');
 const router = require('express').Router();
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'FormationBD',
-    port: 8889
-  });
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.PORT,
+});
 
-  connection.connect();
-  
+connection.connect();
+
 //autentification d'un compte.
 router.post('/login/Connexion', function (req, res) {
-    console.log("post appelé pour l'authentification");
-    console.log(req.body);
-    connection.query(
-      'SELECT pseudo , motdepasse FROM Compte where pseudo = "?" and motdepasse = "?";',
-      [req.body.pseudo, req.body.motdepasse],
-      function (err, result) {
-        if (err) {
-          return res.json({ error: true, message: 'Error occurred' + err });
-        } else {
-          console.log('compte connecté');
-          res.json({ Connexion: 'ok' });
-        }
+  console.log("post appelé pour l'authentification");
+  console.log(req.body);
+  connection.query(
+    'SELECT pseudo , motdepasse FROM Compte where pseudo = "?" and motdepasse = "?";',
+    [req.body.pseudo, req.body.motdepasse],
+    function (err, result) {
+      if (err) {
+        return res.json({ error: true, message: 'Error occurred' + err });
+      } else {
+        console.log('compte connecté');
+        res.json({ Connexion: 'ok' });
       }
-    );
-  });
-  
-  //recuperation des infos d'un compte.
-  router.get('/infosDeCompte/:pseudo', function (req, res) {
-    console.log("get appelé recuperation des infos d'un compte.");
-    console.log(req.params);
-    connection.query(
-      "SELECT * FROM  Compte where pseudo = '" + req.params.pseudo + "'",
-      function (error, results, fields) {
-        if (error) throw error;
-        res.json(results[0]);
-      }
-    );
-  });
-  
-  //creation d'un compte dans la base de données
-  router.post('/creationDeCompte', function (req, res) {
-    console.log("post appelé pour la creation d'un compte");
-    console.log(req.body);
-    connection.query(
-      'insert into Compte (pseudo,motdepasse) values (?,?);',
-      [req.body.pseudo, req.body.motdepasse],
-      function (err, result) {
-        if (err) throw err;
-        console.log('compte inséré');
-        res.json({ 'Compte créé ': 'Ok' });
-      }
-    );
-  });
-  
-  //modification d'un compte dans la base de données
-  router.put('/modificationDeCompte', (req, res) => {
-    var sql =
-      "UPDATE COMPTE SET pseudo='" +
-      req.body.pseudo +
-      "', motdepasse='" +
-      req.body.motdepasse +
-      "' WHERE pseudo=" +
-      req.params.pseudo;
-    connection.query(sql, (err, results) => {
+    }
+  );
+});
+
+//recuperation des infos d'un compte.
+router.get('/infosDeCompte/:pseudo', function (req, res) {
+  console.log("get appelé recuperation des infos d'un compte.");
+  console.log(req.params);
+  connection.query(
+    "SELECT * FROM  Compte where pseudo = '" + req.params.pseudo + "'",
+    function (error, results, fields) {
+      if (error) throw error;
+      res.json(results[0]);
+    }
+  );
+});
+
+//creation d'un compte dans la base de données
+router.post('/creationDeCompte', function (req, res) {
+  console.log("post appelé pour la creation d'un compte");
+  console.log(req.body);
+  connection.query(
+    'insert into Compte (pseudo,motdepasse) values (?,?);',
+    [req.body.pseudo, req.body.motdepasse],
+    function (err, result) {
       if (err) throw err;
-      res.send(results);
-    });
+      console.log('compte inséré');
+      res.json({ 'Compte créé ': 'Ok' });
+    }
+  );
+});
+
+//modification d'un compte dans la base de données
+router.put('/modificationDeCompte', (req, res) => {
+  var sql =
+    "UPDATE COMPTE SET pseudo='" +
+    req.body.pseudo +
+    "', motdepasse='" +
+    req.body.motdepasse +
+    "' WHERE pseudo=" +
+    req.params.pseudo;
+  connection.query(sql, (err, results) => {
+    if (err) throw err;
+    res.send(results);
   });
-  
-  //Supprime d'un compte dans la base de données
-  router.delete('/supprimeDeCompte/:pseudo', (req, res) => {
-    var sql = 'DELETE FROM Compte WHERE  pseudo=' + req.params.pseudo;
-    connection.query(sql, (err, results) => {
-      if (err) throw err;
-      res.json({ 'Compte supprimé ': 'Ok' });
-    });
+});
+
+//Supprime d'un compte dans la base de données
+router.delete('/supprimeDeCompte/:pseudo', (req, res) => {
+  var sql = 'DELETE FROM Compte WHERE  pseudo=' + req.params.pseudo;
+  connection.query(sql, (err, results) => {
+    if (err) throw err;
+    res.json({ 'Compte supprimé ': 'Ok' });
   });
-  
-  module.exports = router;
-  
+});
+
+module.exports = router;
