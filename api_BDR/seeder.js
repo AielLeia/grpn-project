d843367c914ffc04ee720fadcf78bf1686dd4629
isamelmohamedbouh <ismaelmedbouh@outlook.fr>
2021-02-26 12:41:45 +0100

    API_BDR: Finission du seeder pour la base de données relationnelle.

--- api_BDR/seeder.js
+++ api_BDR/seeder.js
@@ -1,36 +1,242 @@
 require('colors');
-const mysqlDriver = require('mysql');
+require('dotenv').config();
+const CryptoJs = require('crypto-js');
+const mysql = require('mysql');
 const util = require('util');
 
-const connection = mysqlDriver.createConnection({
-  host: 'localhost',
-  user: 'springstudent',
-  password: 'Springstudent_98',
-  database: 'FormationBD',
-  port: 3306,
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.PASSWORD,
+  port: parseInt(process.env.PORT),
 });
 
+/// ========================================================================
+/// Définition des données à insérer
+/// ========================================================================
+
+/**
+ * @type {{id: number, pseudo: string, motDePasse: string, nom: string, prenom: string, adresseMail: string}[]}
+ */
 const comptes = [
   {
+    id: 1,
     pseudo: 'junior',
-    motDePasse: 'junior',
+    motDePasse: CryptoJs.AES.encrypt('junior', 'test123').toString(),
     nom: 'ELENGA JUNIOR',
     prenom: 'Junior',
-    addresseMail: 'juniorelengaalphonse@gmail.com',
+    adresseMail: 'juniorelengaalphonse@gmail.com',
   },
   {
+    id: 2,
     pseudo: 'ismed',
-    motDePasse: 'ismael',
+    motDePasse: CryptoJs.AES.encrypt('ismael', 'test123').toString(),
     nom: 'MOHAMED BOUH',
     prenom: 'Ismael',
-    addresseMail: 'ismael@gmail.com',
+    adresseMail: 'ismael@gmail.com',
   },
   {
+    id: 3,
     pseudo: 'ali',
-    motDePasse: 'ali',
+    motDePasse: CryptoJs.AES.encrypt('ali', 'test123').toString(),
     nom: 'HADJ-SAID',
     prenom: 'Ali',
-    addresseMail: 'ali@gmail.com',
+    adresseMail: 'ali@gmail.com',
+  },
+];
+
+/**
+ * @type {{id: number, nom: string}[]}
+ */
+const niveauFormations = [
+  {
+    id: 1,
+    nom: 'Terminal',
+  },
+  {
+    id: 2,
+    nom: 'Première',
+  },
+  {
+    id: 3,
+    nom: 'Seconde',
+  },
+];
+
+/**
+ * @type {{id: number, nom: string, compte_id: number}[]}
+ */
+const moduleFormations = [
+  {
+    id: 1,
+    nom: 'Physique',
+    compte_id: 1,
+  },
+  {
+    id: 2,
+    nom: 'Svt',
+    compte_id: 2,
+  },
+  {
+    id: 3,
+    nom: 'Maths',
+    compte_id: 3,
+  },
+];
+
+/**
+ * @type {{niveau_formation_id: number, module_formation_id: number}[]}
+ */
+const niveauFormationsModuleFormmations = [
+  {
+    niveau_formation_id: 1,
+    module_formation_id: 1,
+  },
+  {
+    niveau_formation_id: 1,
+    module_formation_id: 2,
+  },
+  {
+    niveau_formation_id: 1,
+    module_formation_id: 3,
+  },
+  {
+    niveau_formation_id: 2,
+    module_formation_id: 1,
+  },
+  {
+    niveau_formation_id: 2,
+    module_formation_id: 2,
+  },
+  {
+    niveau_formation_id: 2,
+    module_formation_id: 3,
+  },
+  {
+    niveau_formation_id: 3,
+    module_formation_id: 1,
+  },
+  {
+    niveau_formation_id: 3,
+    module_formation_id: 2,
+  },
+  {
+    niveau_formation_id: 3,
+    module_formation_id: 3,
+  },
+];
+
+/**
+ * @type {{id: numbeer, nom: string, compte_id: number, module_formation_id: number}[]}
+ */
+const unitePedagogiques = [
+  {
+    id: 1,
+    nom: 'Chapitre 1 - Introduction',
+    compte_id: 1,
+    module_formation_id: 1,
+  },
+  {
+    id: 2,
+    nom: 'Chapitre 2 - Ondes et matière',
+    compte_id: 1,
+    module_formation_id: 1,
+  },
+  {
+    id: 3,
+    nom: 'Chapitre 3 - Lois et modèle',
+    compte_id: 1,
+    module_formation_id: 1,
+  },
+  {
+    id: 4,
+    nom: 'Chapitre 4 - Défis du XX1e siècle',
+    compte_id: 1,
+    module_formation_id: 1,
+  },
+  {
+    id: 5,
+    nom: 'Chapitre 5 - Physique quantite',
+    compte_id: 1,
+    module_formation_id: 1,
+  },
+  {
+    id: 6,
+    nom: 'Chapitre 6 - Introduction à la chimie organique',
+    compte_id: 1,
+    module_formation_id: 1,
+  },
+  {
+    id: 7,
+    nom: 'Chapitre 7 - Les transformations chimiques',
+    compte_id: 1,
+    module_formation_id: 1,
+  },
+  {
+    id: 8,
+    nom: 'Chapitre 1 - Nombre et calculs',
+    compte_id: 3,
+    module_formation_id: 3,
+  },
+  {
+    id: 9,
+    nom: 'Chapitre 2 - Géométrie',
+    compte_id: 3,
+    module_formation_id: 3,
+  },
+  {
+    id: 10,
+    nom: 'Chapitre 3 - Fonctions',
+    compte_id: 3,
+    module_formation_id: 3,
+  },
+  {
+    id: 11,
+    nom: 'Chapitre 4 - Statistiques et probabilités',
+    compte_id: 3,
+    module_formation_id: 3,
+  },
+  {
+    id: 12,
+    nom: 'Chapitre 5 - Algorithmique et programmation',
+    compte_id: 3,
+    module_formation_id: 3,
+  },
+  {
+    id: 13,
+    nom: 'Chapitre 1 - Le patrimoine génétique',
+    compte_id: 2,
+    module_formation_id: 2,
+  },
+  {
+    id: 14,
+    nom: 'Chapitre 2 - La tectonique des plaques',
+    compte_id: 2,
+    module_formation_id: 2,
+  },
+  {
+    id: 15,
+    nom: "Chapitre 3 - Nourrir l'humanité",
+    compte_id: 2,
+    module_formation_id: 2,
+  },
+  {
+    id: 16,
+    nom: 'Chapitre 4 - Féminin et masculin',
+    compte_id: 2,
+    module_formation_id: 2,
+  },
+  {
+    id: 17,
+    nom: 'Chapitre 5 - Les variations génétiques et la santé',
+    compte_id: 2,
+    module_formation_id: 2,
+  },
+  {
+    id: 18,
+    nom: "Chapitre 6 - De l'œil au cerveau",
+    compte_id: 2,
+    module_formation_id: 2,
   },
 ];
 
@@ -39,72 +245,145 @@ const main = async () => {
   /** @type {mysqlDriver.QueryFunction} */
   const query = util.promisify(connection.query).bind(connection);
 
-  await query('set foreign_key_checks=0'); // Désactivation de la vérification des clé étrangère
-
   /// ========================================================================
-  /// Destruction puis ré-création de toute les tables de la base de données.
+  /// Destuction de la base de donnée, puis ré-création de de celle-ci
   /// ========================================================================
-  await query('DROP TABLES IF EXISTS Compte');
-  await query('DROP TABLES IF EXISTS Compte_has_Messages');
-  await query('DROP TABLES IF EXISTS Messages');
-  await query('DROP TABLES IF EXISTS ModuleFormation');
-  await query('DROP TABLES IF EXISTS NiveauFormation');
-  await query('DROP TABLES IF EXISTS UnitePedagogique');
-  await query('DROP TABLES IF EXISTS UnitePedagogique_has_NiveauFormation');
+  await query(`DROP DATABASE IF EXISTS ${process.env.DATABASE}`);
+  await query(`CREATE DATABASE ${process.env.DATABASE}`);
+  await query(`USE ${process.env.DATABASE}`);
   console.log(
-    'Destruction puis ré-création de toute les tables de la base de données.'
+    `Destuction de la base de donnée, puis ré-création de de celle-ci: ${process.env.DATABASE}`
       .bgGreen.white.bold
   );
+
+  await query('set foreign_key_checks=0'); // Désactivation de la vérification des clé étrangère
 
   /// ========================================================================
   /// Construction de la table des comptes
   /// ========================================================================
   await query(
-    'CREATE TABLE `Compte` (' +
-      '`pseudo` varchar(45) NOT NULL,' +
-      '`motDePasse` varchar(256) DEFAULT NULL,' +
-      '`nom` varchar(45) DEFAULT NULL,' +
-      '`prenom` varchar(45) DEFAULT NULL,' +
-      '`adresseMail` varchar(45) DEFAULT NULL' +
-      ') ENGINE=InnoDB DEFAULT CHARSET=utf8;'
+    `CREATE TABLE compte (
+        id INT(11) NOT NULL AUTO_INCREMENT,
+        pseudo varchar(255) NOT NULL,
+        motDePasse varchar(255) NOT NULL,
+        nom varchar(255) NOT NULL,
+        prenom varchar(255) NOT NULL,
+        adresseMail varchar(255) NOT NULL,
+        admin TINYINT(1) NOT NULL DEFAULT 0,
+        CONSTRAINT PK_id PRIMARY KEY (id),
+        CONSTRAINT UNIQUE_pseudo UNIQUE (pseudo)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;`
   );
   console.log('Contruction de la table des comptes: '.underline.cyan.bold);
   for (const c of comptes) {
     await query(`
-      INSERT INTO Compte (pseudo, motDePasse, nom, prenom, adresseMail) VALUES
-      ('${c.pseudo}', '${c.motDePasse}', '${c.nom}', '${c.prenom}', '${c.addresseMail}');
+      INSERT INTO compte (id, pseudo, motDePasse, nom, prenom, adresseMail) VALUES
+      (${c.id}, "${c.pseudo}", "${c.motDePasse}", "${c.nom}", "${c.prenom}", "${c.addresseMail}");
     `);
     console.log(
-      `\t-> Insertion: ('${c.pseudo}', '${c.motDePasse}', '${c.nom}', '${c.prenom}', '${c.addresseMail}')`
+      `\t-> Insertion: (${c.id}, ${c.pseudo}", "${c.motDePasse}", "${c.nom}", "${c.prenom}", "${c.addresseMail}")`
         .green.bold
     );
   }
 
   /// ========================================================================
-  /// Construction de la table des messages
+  /// Construction de la table des module de formation
   /// ========================================================================
   await query(`
-    CREATE TABLE Messages (
-      idMessages int(11) NOT NULL,
-      messageEnvoye varchar(45) DEFAULT NULL,
-      messageRecu varchar(45) DEFAULT NULL
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-  `);
-  console.log('Construction de la table des messages: '.underline.cyan.bold);
-
-  /// ========================================================================
-  /// Construction de la table de jointure entre compte et message
-  /// ========================================================================
-  await query(`
-    CREATE TABLE Compte_has_Messages (
-      Compte_pseudo varchar(45) NOT NULL,
-      Messages_idMessages int(11) NOT NULL
+    CREATE TABLE module_formation (
+      id int(11) NOT NULL AUTO_INCREMENT,
+      nom varchar(255) DEFAULT NULL,
+      compte_id int(11) NOT NULL,
+      CONSTRAINT PK_id PRIMARY KEY (id),
+      CONSTRAINT UNIQUE_nom UNIQUE (nom)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
   `);
   console.log(
-    'Construction de la table de jointure entre compte et message: '.underline
-      .cyan.bold
+    'Contruction de la table des modules des formation: '.underline.cyan.bold
   );
+  for (const mf of moduleFormations) {
+    await query(`
+      INSERT INTO module_formation (id, nom, compte_id) VALUES
+      (${mf.id}, "${mf.nom}", ${mf.compte_id});
+    `);
+    console.log(
+      `\t-> Insertion: (${mf.id}, "${mf.nom}", ${mf.compte_id})`.green.bold
+    );
+  }
+
+  /// ========================================================================
+  /// Construction de la table des niveaeux de formations
+  /// ========================================================================
+  await query(`
+    CREATE TABLE niveau_formation (
+      id int(11) NOT NULL AUTO_INCREMENT,
+      nom varchar(255) NOT NULL,
+      CONSTRAINT PK_id PRIMARY KEY (id),
+      CONSTRAINT UNIQUE_nom UNIQUE (nom)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `);
+  console.log(
+    'Contruction de la table des niveaux de formations: '.underline.cyan.bold
+  );
+  for (const nf of niveauFormations) {
+    await query(`
+      INSERT INTO niveau_formation (id, nom) VALUES
+      (${nf.id}, "${nf.nom}");
+    `);
+    console.log(`\t-> Insertion: (${nf.id}, "${nf.nom}")`.green.bold);
+  }
+
+  /// ========================================================================
+  /// Construction de la table de jointure entre les niveaux de formations
+  /// et les modules de formations
+  /// ========================================================================
+  await query(`
+    CREATE TABLE niveau_formation_module_formation (
+      niveau_formation_id int(11) NOT NULL,
+      module_formation_id int(11) NOT NULL,
+      CONSTRAINT PK_niveau_formation_id_module_formation_id PRIMARY KEY (niveau_formation_id, module_formation_id)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `);
+  console.log(
+    'Contruction de la table des niveaux de formations: '.underline.cyan.bold
+  );
+  for (const nf_mf of niveauFormationsModuleFormmations) {
+    await query(`
+      INSERT INTO niveau_formation_module_formation (niveau_formation_id, module_formation_id) VALUES
+      (${nf_mf.niveau_formation_id}, ${nf_mf.module_formation_id});
+    `);
+    console.log(
+      `\t-> Insertion: (${nf_mf.niveau_formation_id}, ${nf_mf.module_formation_id})`
+        .green.bold
+    );
+  }
+
+  /// ========================================================================
+  /// Construction de la table des unités pédagogiques
+  /// ========================================================================
+  await query(`
+    CREATE TABLE unite_pedagogique (
+      id int(11) NOT NULL AUTO_INCREMENT,
+      nom varchar(255) NOT NULL,
+      compte_id int(11) NOT NULL,
+      module_formation_id int(11) NOT NULL,
+      CONSTRAINT PK_id PRIMARY KEY (id),
+      CONSTRAINT UNIQUE_nom UNIQUE(nom)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `);
+  console.log(
+    'Contruction de la table des niveaux de formations: '.underline.cyan.bold
+  );
+  for (const up of unitePedagogiques) {
+    await query(`
+      INSERT INTO unite_pedagogique (id, nom, compte_id, module_formation_id) VALUES
+      (${up.id}, "${up.nom}", ${up.compte_id}, ${up.module_formation_id});
+    `);
+    console.log(
+      `\t-> Insertion: (${up.id}, "${up.nom}", ${up.compte_id}, ${up.module_formation_id})`
+        .green.bold
+    );
+  }
 
   await query('set foreign_key_checks=1'); // Réactivation de la vérifcation des clès étrangère
 };
