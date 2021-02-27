a34130485660cc323c84a44dea51e75a4ab9e59e
Ali-hdj <63148117+Ali-hdj@users.noreply.github.com>
2021-02-27 23:42:06 +0100

    Update app.js

--- api_MONGO/app.js
+++ api_MONGO/app.js
@@ -158,9 +158,3 @@ app.use(function (req, res, next) {
 
 
 
-/**
- * lancement du serveur sur le port 3000
- */
-  app.listen(3000, function() {
-    console.log('Example app listening on port 3000!');
-  });
